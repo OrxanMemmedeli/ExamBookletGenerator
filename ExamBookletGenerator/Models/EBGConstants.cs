ab36aff849a941c260b9d867f713cf966487ea10
orxan.m <99455@ORXAN>
2024-11-26 16:36:09 +0400

    added partialView və Admin template

---
+++ ExamBookletGenerator/Models/EBGConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace ExamBookletGenerator.Models
+{
+    public static class EBGConstants
+    {
+        public static class Sections
+        {
+            public const string AdminScripts = "AdminScripts";
+            public const string TeacherScripts = "TeacherScripts";
+            public const string MainScripts = "MainScripts";
+
+
+            public const string AdminStyles = "AdminStyles";
+            public const string TeacherStyles = "TeacherStyles";
+            public const string MainStyles = "MainStyles";
+        }
+
+    }
+}
